2667ca37ecb453758fe5de9e4b7dbfcba10eae6d
Heladio Arteaga Franco <heladiofog@gmail.com>
2024-02-08 14:12:27 -0600

    Adding before and after all ssetup and teardown calls.

--- src/02-setup-teardown/01-setup-teardown.test.js
+++ src/02-setup-teardown/01-setup-teardown.test.js
@@ -3,16 +3,32 @@ const { sum } = require('../03-unit-tests/math');
 
 let result;
 
+beforeAll(() => {
+  // Configuración antes de todas las pruebas
+  console.log('BeforeAll');
+});
+
 beforeEach(() => {
   // Configuración antes de cada prueba
   result = sum(2, 3);
+  console.log('BeforeEach');
 });
 
 afterEach(() => {
   // Limpiar después de cada prueba
   result = undefined;
+  console.log('AfterEach');
+});
+
+afterAll(() => {
+  // Limpiar después de todas las pruebas
+  console.log('AfterAll');
 });
 
 test('Suma de dos números', () => {
+  expect(result).toBe(5);
+});
+
+test('Suma de dos números 2 ', () => {
   expect(result).toBe(5);
 });
